795da32cfe73440f00792c919d2a2bee224ed087
Pierre Marien <pierremarien29@gmail.com>
2023-10-14 01:41:16 +0200

    wip

--- src/components/attendees-table-injector.js
+++ src/components/attendees-table-injector.js
@@ -1,43 +1,38 @@
 import { getAllAttendees } from "../queries/get-all-attendees.js";
-import { getAllEvent } from "../queries/get-all-event.js";
 
+export async function createTables(eventData, container) {
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tbody = document.createElement("tbody");
+  
+    // Headers
+    const headerRow = document.createElement("tr");
+    thead.appendChild(headerRow);
 
-export async function createTables() {
-    const tablesContainer = document.getElementsByClassName("tables-container");
+    const attendeesHeader = document.createElement("th");
+    attendeesHeader.textContent = "Attendees";
+    headerRow.appendChild(attendeesHeader);
 
-    const attendees = await getAllAttendees();
-    const events = await getAllEvent();
-
-    events.forEach((event) => {
-      const table = document.createElement("table");
-      const thead = document.createElement("thead");
-      const tbody = document.createElement("tbody");
-
-      
-      const headerRow = document.createElement("tr");
-      headerRow.innerHTML = `
-        <th>Attendees</th>
-        ${event.dates.map((dateItem) => `<th>${dateItem.date}</th>`).join('')}
-      `;
-      thead.appendChild(headerRow);
-
-      
-      event.dates.forEach((dateItem) => {
-        const row = document.createElement("tr");
-        row.innerHTML = `<td>${dateItem.available ? 'V' : 'X'}</td>`;
-        attendees.forEach((attendee) => {
-          const attendeeDate = attendee.dates.find((date) => date.date === dateItem.date);
-          row.innerHTML += `<td>${attendeeDate ? (attendeeDate.attendees ? 'V' : 'X') : 'X'}</td>`;
-        });
-        tbody.appendChild(row);
-      });
-
-      table.appendChild(thead);
-      table.appendChild(tbody);
-      tablesContainer.appendChild(table);
-    });
+    for (let i = 0, length = eventData.dates.length; i < length; i++) {
+      const date = eventData.dates[i];
+      const dateHeader = document.createElement("th");
+      dateHeader.textContent = date;
+      headerRow.appendChild(dateHeader);
+    }
+  
+    // Body
+   
+  
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    container.appendChild(table);
   }
-
+  
+  
+  
+  
+  
+  
  
 
 
