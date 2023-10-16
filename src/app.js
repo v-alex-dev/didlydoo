1b5f90f35a88bb71dedd21d1d58492b4c727ef17
Alexandre <vensalex1991@gmail.com>
2023-10-16 15:35:57 +0200

    fix bug

--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
 
-import { btnModalPatchevent } from "./components/btn-modal-patch-event.js";
 // import { getAllAttendees } from "./queries/get-all-attendees.js";
 import { getAllEvents } from "./queries/get-all-events.js"
 import { displayEvents } from "./components/event-datas.js";
 
 const event = getAllEvents();
-
-event.then(data => {
-  console.log(data);
-	data.forEach(element => {
-    btnModalPatchevent(element);
-  });
-
-})
 
 displayEvents();
