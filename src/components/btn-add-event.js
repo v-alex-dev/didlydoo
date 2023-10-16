e717c1def5757f1ba1932e95da5ffd95e134bb25
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-10-16 16:01:27 +0200

    del modal

--- src/components/btn-add-event.js
+++ src/components/btn-add-event.js
@@ -6,8 +6,8 @@ export const btnAddEvent = () => {
   head.appendChild(title);
   const btnAdd = document.createElement("button");
   btnAdd.textContent = "Add Event";
-
   head.appendChild(btnAdd);
+  // event button add
   btnAdd.addEventListener("click", () => {
     let modal = document.createElement("div");
     head.appendChild(modal);
@@ -29,29 +29,29 @@ export const btnAddEvent = () => {
     inputDesc.setAttribute("name", "description");
     inputDesc.setAttribute("type", "text");
     inputDescLabel.appendChild(inputDesc);
-    // let inputDateLabel = document.createElement("label");
     let inputDate = document.createElement("input");
     inputDate.setAttribute("type", "number");
-    // inputDateLabel.appendChild(inputDate);
     inputDate.setAttribute("type", "number");
     let formSend = document.createElement("form");
     let inputSend = document.createElement("button");
     inputSend.textContent = "Send";
 
+    // appendChild
     modal.appendChild(formSend);
-
     formSend.appendChild(inputNameLabel);
     formSend.appendChild(inputAuthorLabel);
     formSend.appendChild(inputDescLabel);
     modal.appendChild(inputDate);
     modal.appendChild(inputSend);
-
+    // event input date
     inputDate.addEventListener("keyup", (c) => {
       if (c.key === "Enter") {
         if (validValentin(inputDate)) {
           for (let i = 0; i < inputDate.value; i++) {
             let inputDateLabel = document.createElement("label");
             let inputDate2 = document.createElement("input");
+            inputDate2.setAttribute("name", "date");
+            inputDate2.classList.add("classDate");
             inputDate2.setAttribute("type", "date");
             inputDateLabel.appendChild(inputDate2);
             formSend.appendChild(inputDate2);
@@ -77,7 +77,7 @@ export const btnAddEvent = () => {
     });
   });
 };
-
+// Validations
 function validValentin(inputDate) {
   let isValid = true;
   if (inputDate.value < 2 || inputDate.value > 12) {
