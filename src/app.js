2c9cd42693356fa72f10769820692f3be48621bc
Alexandre <vensalex1991@gmail.com>
2023-10-16 09:32:53 +0200

    merging pierre && bug fix

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 
 import { btnModalPatchevent } from "./components/btn-modal-patch-event.js";
 import { getAllAttendees } from "./queries/get-all-attendees.js";
-import { getAllEvents } from "./queries/get-all-events.js";
+import { getAllEvents } from "./queries/get-all-events.js"
 import { displayEvents } from "./components/event-datas.js";
-
 
 const event = getAllEvents();
 
