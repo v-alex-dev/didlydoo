1702c567391acf09b5d1c035be53f6ca138eb777
Alexandre <vensalex1991@gmail.com>
2023-10-18 08:51:17 +0200

    add delete btn

--- src/components/event-datas.js
+++ src/components/event-datas.js
@@ -4,6 +4,7 @@ import { getAllAttendees } from "../queries/get-all-attendees.js";
 import { createTables } from "./attendees-table-injector.js";
 import { btnModalPatchevent } from "./btn-modal-patch-event.js";
 import { createForm } from "./PostInput.js";
+import { btnDelete } from "./btn-delete.js";
 
 // export + function
 export async function displayEvents() {
@@ -17,18 +18,22 @@ export async function displayEvents() {
     const eventDataSection = document.createElement("section");
     mainHtml.appendChild(eventDataSection);
     eventDataSection.classList.add("section-data");
+    const eventDivMenu = document.createElement('div');
 
     const eventDataDiv = document.createElement("div");
     const eventDataH2 = document.createElement("h2");
     const eventDatah3 = document.createElement('h3')
     const eventDataP = document.createElement("p");
     const eventUpdate = btnModalPatchevent(eventData);
+    const btnDeleteEvent = btnDelete(eventData.id);
     // append child
     eventDataSection.appendChild(eventDataDiv);
     eventDataDiv.classList.add('section-header')
     eventDataH2.textContent = eventData.name;
     eventDatah3.textContent = eventData.author;
-    eventDataDiv.appendChild(eventUpdate);
+    eventDataDiv.appendChild(eventDivMenu);
+    eventDivMenu.appendChild(eventUpdate);
+    eventDivMenu.appendChild(btnDeleteEvent)
     eventDataDiv.appendChild(eventDataH2);
     eventDataDiv.appendChild(eventDatah3);
     eventDataP.textContent = eventData.description;
